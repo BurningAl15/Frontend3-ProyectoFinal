3ad972dc384d6f108f3980b561ce150e55c1b6ce
Aldhair Vera <aldhairvera-externo@tgacompany.com>
2024-09-07 09:04:28 -0500

    Add form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,12 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
+
+const initialState = {
+  name: "",
+  email: ""
+}
 
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
+  const [user, setUser] = useState(initialState);
+  const [errors, setErrors] = useState(initialState);
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setUser({ ...user, [name]: value })
+
+    validateField(name, value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  }
+
+  const validateField = (fieldName, value) => {
+    let error = '';
+    const nameRegex = /^[a-zA-Z]+ [a-zA-Z]+$/;
+
+    switch (fieldName) {
+      case 'name':
+        if (!nameRegex.test(value)) {
+          error = 'Please enter a full name (first and last)';
+        }
+        break;
+      case 'email':
+        if (value === '' && user.email.trim().length > 3) {
+          error = 'Please enter an email';
+        }
+        break;
+      default:
+        break;
+    }
+    setErrors({ ...errors, [fieldName]: error });
+  };
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
+        <div className='section'>
+          <label htmlFor="name">Full Name:</label>
+          <input type="text" name='name' id="name" value={user.name} onChange={handleChange} />
+          {errors.name && <span style={{ color: "red" }}>{errors.name}</span>}
+        </div>
+
+        <div className='section'>
+          <label htmlFor="email">Email:</label>
+          <input type="text" name='email' id="email" value={user.email} onChange={handleChange} />
+          {errors.email && <span style={{ color: "red" }}>{errors.email}</span>}
+        </div>
+        <button type="submit">Send</button>
       </form>
     </div>
   );
